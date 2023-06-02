595372ec0dd749faa847eb028eec98b08272334e
JennerMaxim <jennersi1remaxim@gmail.com>
2023-06-02 14:12:53 +0300

    JavaScript Hoisting / JavaScript Use Strict

--- access/index1.js
+++ access/index1.js
@@ -1,8 +1,29 @@
-scopeFunction()
-document.getElementById("scopes").innerHTML = "Hi " + carName;
-function scopeFunction(){
-    carName = "Bugatti";
+// "use strict";
+// tom = 12;
+scopeFunction();
+// document.getElementById("scope").innerHTML = "Hi " + carName;
+function scopeFunction() {
+  // "use strict"
+  //   carName = "Bugatti";
 }
 
-var scope1 = "Maxim"
+var scope1 = "Maxim";
 document.getElementById("scope").innerHTML = window.scope1;
+
+eval("var x = 2");
+alert(x);
+
+// with (Math){x = cos(2)};
+delete Object.prototype;
+var obj = {
+  get x() {
+    return 0;
+  },
+};
+// Object.defineProperty(obj, "x", {value: 0, writable: false});
+obj.x = 1;
+
+function strictFunction(){
+    alert(this)
+}
+strictFunction();
