fb881b5a986e5f3dd62a9f84772752888fa1172b
JennerMaxim <jennersi1remaxim@gmail.com>
2023-06-03 12:23:40 +0300

    The JavaScript this Keyword

--- access/index1.js
+++ access/index1.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 // tom = 12;
 scopeFunction();
 // document.getElementById("scope").innerHTML = "Hi " + carName;
@@ -11,19 +11,50 @@ var scope1 = "Maxim";
 document.getElementById("scope").innerHTML = window.scope1;
 
 eval("var x = 2");
-alert(x);
+// alert(x);
 
 // with (Math){x = cos(2)};
-delete Object.prototype;
+/* delete Object.prototype;
 var obj = {
   get x() {
     return 0;
   },
-};
+}; */
 // Object.defineProperty(obj, "x", {value: 0, writable: false});
-obj.x = 1;
+// obj.x = 1;
 
-function strictFunction(){
-    alert(this)
+function strictFunction() {
+  alert(this);
 }
-strictFunction();
+// strictFunction();
+
+/* var x = this;
+document.getElementById("this").innerHTML = x; */
+
+function thisFunction() {
+  // "use strict";
+  var person = {
+    fname: "Kagheni",
+    lname: "Maximillien",
+    id: 118887,
+    fullname: function () {
+      return this;
+    },
+  };
+  /*   var x = this;
+  function thisInFunction(){
+    return this
+  } */
+
+  var person1 = {
+    fullname: function () {
+      return this.fname + " " + this.lname;
+    },
+  };
+  var person2 = {
+    fname: "Kagheni",
+    lname: "Maximillien",
+  };
+  var x = person1.fullname.call(person2);
+  document.getElementById("this").innerHTML = x;
+}
