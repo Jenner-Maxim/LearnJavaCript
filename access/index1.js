51ae64f1f725f7467a208ac35040f9b4d877864f
JennerMaxim <jennersi1remaxim@gmail.com>
2023-06-05 17:45:19 +0300

    JavaScript Common Mistakes

--- access/index1.js
+++ access/index1.js
@@ -165,3 +165,10 @@ let bestPractices = () => {
   };
   document.getElementById("best").innerHTML = bestPractices(4);
 };
+
+let commonMistakes = () => {
+  var x = 0.1;
+  var y = 0.2;
+  var z = (x * 10 + y * 10) / 10;
+  document.getElementById("mistake").innerHTML = typeof myobj !== "undefined" && myobj !==null;
+}
