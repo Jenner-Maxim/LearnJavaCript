d153680c1f33a76e805db4b2cfd9ce6f1fa52062
JennerMaxim <jennersi1remaxim@gmail.com>
2023-06-07 10:52:52 +0300

    ECMAScript 5 - JavaScript 5

--- access/index1.js
+++ access/index1.js
@@ -7,11 +7,11 @@ function scopeFunction() {
   //   carName = "Bugatti";
 }
 
-var scope1 = "Maxim";
-document.getElementById("scope").innerHTML = window.scope1;
+// var scope1 = "Maxim";
+// document.getElementById("scope").innerHTML = window.scope1;
 
-eval("var x = 2");
-// alert(x);
+// eval("var x = 2");
+// // alert(x);
 
 // with (Math){x = cos(2)};
 /* delete Object.prototype;
@@ -92,26 +92,26 @@ const arrowFunction = () => {
   document.getElementById("arrow").innerHTML = hello("Arrow Function!");
 };
 
-var helloworld;
-hello = function () {
-  document.getElementById("regular").innerHTML += this;
-};
-// hello();
+// var helloworld;
+// hello = function () {
+//   document.getElementById("regular").innerHTML += this;
+// };
+// // hello();
 
-window.addEventListener("load", hello);
+// window.addEventListener("load", hello);
 
-document
-  .getElementById("regularFunction")
-  .addEventListener("click", helloworld);
-document
-  .getElementById("regularFunction")
-  .addEventListener("click", arrowFunction);
+// document
+//   .getElementById("regularFunction")
+//   .addEventListener("click", helloworld);
+// document
+//   .getElementById("regularFunction")
+//   .addEventListener("click", arrowFunction);
 
-hello = () => {
-  document.getElementById("regular").innerHTML += this;
-};
-window.addEventListener("load", hello);
-document.getElementById("regularFunction").addEventListener("click", hello);
+// hello = () => {
+//   document.getElementById("regular").innerHTML += this;
+// };
+// window.addEventListener("load", hello);
+// document.getElementById("regularFunction").addEventListener("click", hello);
 
 function classFunction() {
   class Car {
@@ -170,5 +170,139 @@ let commonMistakes = () => {
   var x = 0.1;
   var y = 0.2;
   var z = (x * 10 + y * 10) / 10;
-  document.getElementById("mistake").innerHTML = typeof myobj !== "undefined" && myobj !==null;
+  document.getElementById("mistake").innerHTML =
+    typeof myobj !== "undefined" && myobj !== null;
+};
+
+let input;
+const getFunction = () => {
+  input = document.getElementById("input").value;
+  document.getElementById("setdata").innerHTML += input;
+  // console.log(input);
+};
+
+// let getFunction2;
+let btn2 = document.getElementById("btn2");
+let setdata2 = document.getElementById("setdata2");
+
+btn2.addEventListener(
+  "click",
+  (getFunction2 = () => {
+    let input2 = document.getElementById("input2").value;
+    setdata2.innerHTML += input2;
+    console.log(input2);
+  })
+);
+
+let get2 = () => {
+  console.log(input2);
+};
+get2();
+
+// ECMAScript  5 - JavaScript 5
+
+// let str = "           Maxim           ";
+// alert(str.trim());
+
+let checkArray = () => {
+  let fruits = ["Banana", "Orange", "Apple", "Mango"];
+  let x = document.getElementById("array");
+  x.innerHTML = Array.isArray(fruits);
+};
+
+let forEachArry = () => {
+  let txt = "";
+  let numbers = [53, 32, 2, 22, 52];
+  numbers.forEach(arrayforEach);
+  document.getElementById("foreach").innerHTML = txt;
+  function arrayforEach(value) {
+    txt = txt + value + "<br/>";
+  }
+};
+
+let mapArray = () => {
+  let number1 = [52, 452, 42, 422, 242];
+  let number2 = number1.map(mapArryFunction);
+
+  document.getElementById("map").innerHTML = number2;
+
+  function mapArryFunction(value) {
+    return value * 2;
+  }
+};
+
+let arrayFilter = () => {
+  let number1 = [23, 5, 2, 36, 13, 34];
+  let over18 = number1.some(filterFunction);
+  document.getElementById("filter").innerHTML = over18;
+  function filterFunction(value) {
+    return value > 18;
+  }
+};
+
+let reduceArray = () => {
+  let number1 = [23, 5, 2, 36, 13, 34];
+  let sum = number1.reduceRight(reduceFunction);
+  document.getElementById("reduce").innerHTML = "The sum is: " + sum;
+  function reduceFunction(total, value) {
+    return total + value;
+  }
+};
+
+let indexOfArray = () => {
+  let fruits = ["Apple", "Orange", "Apple", "Mango"];
+  let a = fruits.lastIndexOf("Apple");
+  document.getElementById("indexof").innerHTML =
+    "Apple is found in position " + a;
+};
+
+let jsonParse = () => {
+  // let txt = '{"name":"Maxim", "age":35, "city": "Kampala"}'
+  let obj = { name: "Maxim", age: 35, city: "Kampala" };
+  var myJSON = JSON.stringify(obj);
+  document.getElementById("json").innerHTML = myJSON;
+};
+
+let getterSetter = () => {
+  // let ffname = document.getElementById("fname").value;
+  // let llname = document.getElementById("lname").value;
+
+  let person = {
+    fname: "Kagheni",
+    lname: "Maxim",
+    language: "NO",
+    // get lang() {
+    //   return this.language;
+    // },
+    // set lang(value) {
+    //   this.language = value.toUpperCase();
+    // },
+  };
+  // person.lang = "en";
+
+  Object.defineProperty(person, "language", {
+    get: function () {
+      return language;
+    },
+    set: function (value) {
+      language = value.toUpperCase();
+    },
+  });
+  // txt = "";
+  // for (let x in person) {
+  //   txt += person[x] + "<br>";
+  // }
+
+  person.language = "en";
+  document.getElementById("getset").innerHTML = person.language;
+};
+
+let fname = "Maxim";
+for (i in fname) {
+  console.log(fname[i]);
+}
+
+let charat = () => {
+  let str = "Hello, Maxim";
+  document.getElementById("charat").innerHTML = str.charAt(4);
 }
