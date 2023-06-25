ed811bf8cd3fc9082db4e5ae813be904d08ca99f
JennerMaxim <jennersi1remaxim@gmail.com>
2023-06-25 13:02:42 +0300

    AJAX Examples

--- access/index1.js
+++ access/index1.js
@@ -969,25 +969,45 @@ function previous() {
 // }
 
 let x, xmlhttp, xmlDoc;
-xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-  if (this.readyState === 4 && this.status === 200) {
-    xmlDoc = xmlhttp.responseXML;
-    x = xmlDoc.getElementsByTagName("CD");
-    table = "<tr><th>Artist</th><th>Title</th><tr>";
-    for (i = 0; i < x.length; i++) {
-      table += "<tr onclick='displayCD(" + i + ")'><td>";
-      table += x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue;
-      table += "</td><td>";
-      table += x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue;
-      table += "</td></tr>";
+let xmlFunc = () => {
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      xmlDoc = xmlhttp.responseXML;
+      x = xmlDoc.getElementsByTagName("CD");
+      table = "<tr><th>Artist</th><th>Title</th><tr>";
+      for (i = 0; i < x.length; i++) {
+        table += "<tr onclick='displayCD(" + i + ")'><td>";
+        table += x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue;
+        table += "</td><td>";
+        table += x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue;
+        table += "</td></tr>";
+      }
+      document.getElementById("demo").innerHTML = table;
     }
-    document.getElementById("demo").innerHTML = table;
-  }
+  };
+  xmlhttp.open("GET", "cd_catalog.xml", true);
+  xmlhttp.send();
 };
-xmlhttp.open("GET", "cd_catalog.xml", true);
-xmlhttp.send();
-
 function displayCD(i) {
-  document.getElementById("showCD").innerHTML = "Artist: " + x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue + "<br>Title: " + x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue + "<br>Year: " + x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue;
+  document.getElementById("showCD").innerHTML =
+    "Artist: " +
+    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+    "<br>Title: " +
+    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+    "<br>Year: " +
+    x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue;
+}
+
+let getXMLdata = (url) => {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200 ){
+      document.getElementById("A1").innerHTML = this.status;
+      document.getElementById("A2").innerHTML = this.statusText;
+      document.getElementById("A3").innerHTML = this.responseText;
+    }
+  }
+  xhttp.open("GET", url, true);
+  xhttp.send();
 }
